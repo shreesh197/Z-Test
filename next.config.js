5818445e89ac0a2dfaa1feef7f43f0e07f978028
Shreesh Tiwari <64853202+shreesh197@users.noreply.github.com>
2021-10-04 13:07:26 +0530

    Pushed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,42 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withOffline = require('next-offline');
 
-const withCSS = require('@zeit/next-css')
+const nextConfig = {
+    target: 'serverless',
 
-module.exports = withCSS({
-  cssLoaderOptions: {
-    url: false
-  }
-});
+    transformManifest: (manifest) => ['/'].concat(manifest), // - add the homepage to the cache
+
+    generateInDevMode: true,
+
+    workboxOpts: {
+        swDest: 'static/service-worker.js',
+        runtimeCaching: [
+            {
+                urlPattern: /^https?.*/,
+                handler: 'NetworkFirst',
+                options: {
+                    cacheName: 'https-calls',
+                    networkTimeoutSeconds: 15,
+                    expiration: {
+                        maxEntries: 150,
+                        maxAgeSeconds: 30 * 24 * 60 * 60 // - 1 month. Change if want.
+                    },
+                    cacheableResponse: {
+                        statuses: [0, 200]
+                    }
+                }
+            }
+        ]
+    },
+
+    // - @shreesh de-comment only for prod.
+    // For <Icon /> support:
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            loader: 'raw-loader'
+        });
+        return config;
+    }
+};
+
+module.exports = withOffline(nextConfig);
